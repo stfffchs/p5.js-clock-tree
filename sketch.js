f18b55645f287bf0a5b85ff1575748989d6ba4b6
stfffchs <st.a.fuchs@gmail.com>
2018-12-19 19:20:40 +0100

    final

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ function setup() {
 }
 
 function draw() {
-	background(0);
+    background(0);
 
-	var sec = second();
+    var sec = second();
     var min = minute();
     var hr = hour();
     var d = day();
@@ -14,51 +14,60 @@ function draw() {
 
     var minRadius = map(min, 0, 59, 60, 119);
     var hrRadius = map(hr, 0, 23, 120, 143)
-    var dRadius = map(day, 1, 31, 144, 175)
-    var mRadius = map(month, 1, 12, 176, 188)
+    var dRadius = map(d, 1, 31, 144, 175)
+    var mRadius = map(m, 1, 12, 176, 188)
 
     var fullscreen = 300 / 60;
 
 
 
     //Month-Underlay
-    fill(50, 100, 200);
+    fill(255, 255, 255, 10);
+    noStroke();
     ellipse(width/2,height/2, 935, 935);
     //Month
     noFill;
-    stroke(0);
+    stroke(255);
     ellipse(width/2,height/2,mRadius * fullscreen,mRadius * fullscreen);
 
+
     //Day-Underlay
-    fill(230, 200, 230);
+    fill(255, 255, 255, 10);
+    stroke(0);
     ellipse(width/2,height/2, 875, 875);
     //Days
     noFill;
-    stroke(0);
+    stroke(255);
     ellipse(width/2,height/2,dRadius * fullscreen,dRadius * fullscreen);
 
+
     //Hours-Underlay
-    fill(224,255,255);
+    fill(255, 255, 255, 10);
+    stroke(0);
     ellipse(width/2,height/2, 720, 720);
     //Hours
     noFill;
-    stroke(0);
+    stroke(255);
     ellipse(width/2,height/2,hrRadius * fullscreen,hrRadius * fullscreen);
 
+
     //Minutes-Underlay
-    fill(72,209,204);
+    fill(255, 255, 255, 10);
+    stroke(0);
     ellipse(width/2,height/2, 600, 600);
     //Minutes
     noFill;
+    stroke(255);
     ellipse(width/2,height/2,minRadius * fullscreen,minRadius * fullscreen);
 
 
     //Seconds Underlay
-    fill(100,149,237);
+    fill(255, 255, 255, 10);
+    stroke(0);
     ellipse(width/2,height/2,300, 300);
     //Seconds
     noFill;
-    stroke(0);
+    stroke(255);
     ellipse(width/2,height/2,sec * fullscreen,sec * fullscreen);
 
 //
